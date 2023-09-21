3ba951cbc2dd707c93bc100b1550adf1ee7135af
Isai Jesus <isaijesus02@gmail.com>
2023-09-20 20:41:32 -0600

    view login 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+i//mport App from './App.jsx'
 import './index.css'
+import Account from './views/Account.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Account />
   </React.StrictMode>,
 )
